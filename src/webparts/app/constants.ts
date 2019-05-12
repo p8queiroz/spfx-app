5142702d970363c590cb8db77867547b24adbf6c
Paulo Queiroz <phqs.phqs@gmail.com>
2019-05-12 19:44:49 -0300

    delete initial project

--- src/webparts/app/constants.ts
+++
@@ -1,4 +0,0 @@
-//export const SPA_URL = process.env.appUrl;
-//export const API_URL = process.env.APIUrl;
-export const site = "Dashboard site";
-export const PERMISSION_TYPES = Object.freeze({ "owner": "owner", "member": "member", "visitor": "visitor" });
--- src/webparts/appHome/components/IAppHomeProps.ts
+++
@@ -1,3 +0,0 @@
-export interface IAppHomeProps {
-  description: string;
-}
